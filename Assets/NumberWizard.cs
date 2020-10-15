6945b2223e5ce8c040920a3ee1b22a853f914e42
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-10-15 14:06:01 -0700

    Update NumberWizard.cs
    Given max and min varaibles

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number...");
-        Debug.Log("The highest number that you can pick is..");
-        Debug.Log("The lowest number that you can pick is..");
+        Debug.Log("The highest number that you can pick is: " + max);
+        Debug.Log("The lowest number that you can pick is: " + min);
     }
 
     // Update is called once per frame
