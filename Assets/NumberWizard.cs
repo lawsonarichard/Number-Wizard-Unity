995b325c26db83e0d211f706078d4e429131eabe
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-10-15 14:21:00 -0700

    Update NumberWizard.cs
    added UpArrow, DownArrow and Return inputs

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -14,11 +14,27 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number...");
         Debug.Log("The highest number that you can pick is: " + max);
         Debug.Log("The lowest number that you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = higher, Push Down = lower, Hit Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        //Detect when the down arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        //Detect when the Return key is pressed down
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
     }
 }
