f466c459ea081ddf13c4690f915ac1dd7690fe24
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-10-15 14:52:05 -0700

    Update NumberWizard.cs
    added user input with feedback and guessing structure

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -13,11 +13,12 @@ public class NumberWizard : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to Number Wizard!");
-        Debug.Log("Pick a number...");
-        Debug.Log("The highest number that you can pick is: " + max);
-        Debug.Log("The lowest number that you can pick is: " + min);
+        Debug.Log("Pick a number...any number, between " + min + " and " + max);
+        Debug.Log("Again, the highest number that you can pick is: " + max);
+        Debug.Log("Once more, the lowest number that you can pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than 500");
-        Debug.Log("Push Up = higher, Push Down = lower, Hit Enter = Correct");
+        Debug.Log("Pressing Up on the keyboard, means that your number = higher, {ressing Down means that your number = lower, If I've guessed correctly kindly press Enter = Correct");
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -26,21 +27,21 @@ public class NumberWizard : MonoBehaviour
         //Detect when the up arrow key is pressed down
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is the number higher or lower than " + guess);
         }
         //Detect when the down arrow key is pressed down
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is the number higher or lower than " + guess);
         }
         //Detect when the Return key is pressed down
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter key was pressed.");
+            Debug.Log("I am certain your number is " + guess);
         }
     }
 }
